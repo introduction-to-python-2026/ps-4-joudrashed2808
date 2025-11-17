3dba9f669edc126932dbec2947c1e83f613c3c6e
Joud Rashed <joudrashed2808@gmail.com>
2025-11-17 19:21:32 +0200

    Add string splitting functions in string_utils.py
    Implemented functions to split strings based on uppercase letters and digits.

--- string_utils.py
+++ string_utils.py
@@ -1,6 +1,40 @@
 def split_before_each_uppercases(formula):
-    pass # Replace the `pass` with your code
+    prefix = ""
+    number_start_index = -1
+
+    for i, char in enumerate(formula):
+        if char.isdigit():
+            number_start_index = i
+            break
+        prefix += char
+
+    if number_start_index == -1:
+        return formula, 1
+    else:
+        number_part = formula[number_start_index:]
+        return prefix, int(number_part)
 
 
 def split_at_first_digit(formula):
-    pass # Replace the `pass` with your code
+    if not formula:
+        return []
+
+    results = []
+    current_chunk_start = 0
+
+    for i in range (1, len(formula)):
+        char = formula[i]
+
+        if char.isupper():
+            chunk = formula[current_chunk_start:i]
+            if chunk:
+                results.append(chunk)
+
+            current_chunk_start = i
+
+    final_chunk = formula[current_chunk_start:]
+    if final_chunk:
+        results.append(final_chunk)
+
+    return results
+    
